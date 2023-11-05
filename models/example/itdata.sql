5b3cc1a23a29a7b458d3625aeff07ced09efe471
Puneet Makhija <puneet.makhija@nagarro.com>
2023-11-06 02:01:52 +0530

    code added

--- models/example/itdata.sql
+++ models/example/itdata.sql
@@ -2,7 +2,7 @@
 
 WITH FilteredData AS (
     SELECT * 
-    FROM Genome.genomeschema.covid_epidemiology C
+    FROM genomeschema.covid_epidemiology C
     WHERE key = 'IT'
 )
 
--- models/example/new_covid_epidemiology.sql
+++ models/example/new_covid_epidemiology.sql
@@ -3,7 +3,7 @@ WITH FilteredData AS (
       cast(key as varchar) as "mykey",
       date as "mydate",
       new_tested as mynew_tested
-      FROM Genome.genomeschema.covid_epidemiology C
+      FROM genomeschema.covid_epidemiology C
       WHERE key = 'IT'
 )
 
